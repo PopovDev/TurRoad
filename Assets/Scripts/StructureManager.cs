7720f394fbeeb4dc3b65c73b1a59738358de2ac7
Andrey <andreypopov2628007@gmail.com>
2021-09-25 00:14:32 +0300

    Structure manager edit

--- Assets/Scripts/StructureManager.cs
+++ Assets/Scripts/StructureManager.cs
@@ -1,38 +1,36 @@
 ﻿using System;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class StructureManager : MonoBehaviour
 {
-    public StructurePrefabWeighted[] housesPrefabs;
-    public StructurePrefabWeighted[] specialPrefabs, bigStructuresPrefabs;
+    public GameObject[] special;
+    public GameObject[] bigStructures;
+    public GameObject[] houses;
     public PlacementManager placementManager;
-
-    private float[] _houseWeights, _specialWeights, _bigStructureWeights;
-
-    private void Start()
-    {
-        _houseWeights = housesPrefabs.Select(prefabStats => prefabStats.weight).ToArray();
-        _specialWeights = specialPrefabs.Select(prefabStats => prefabStats.weight).ToArray();
-        _bigStructureWeights = bigStructuresPrefabs.Select(prefabStats => prefabStats.weight).ToArray();
-    }
 
     public void PlaceHouse(Vector3Int position)
     {
         if (!CheckPositionBeforePlacement(position)) return;
-        var randomIndex = GetRandomWeightedIndex(_houseWeights);
-        placementManager.PlaceObjectOnTheMap(position, housesPrefabs[randomIndex].prefab, CellType.Structure);
+        placementManager.PlaceObjectOnTheMap(position, houses[Random.Range(0, houses.Length)], CellType.Structure);
     }
 
-    internal void PlaceBigStructure(Vector3Int position)
+    public void PlaceBigStructure(Vector3Int position)
     {
         const int width = 2;
         const int height = 2;
         if (!CheckBigStructure(position, width, height)) return;
-        var randomIndex = GetRandomWeightedIndex(_bigStructureWeights);
-        placementManager.PlaceObjectOnTheMap(position, bigStructuresPrefabs[randomIndex].prefab, CellType.Structure, width, height);
+        placementManager.PlaceObjectOnTheMap(position, bigStructures[Random.Range(0, bigStructures.Length)], CellType.Structure, width, height);
     }
-
+    public void PlaceSpecial(Vector3Int position)
+    {
+        if (!CheckPositionBeforePlacement(position)) return;
+        placementManager.PlaceObjectOnTheMap(position,
+            special[Random.Range(0,special.Length)],
+            CellType.SpecialStructure);
+    }
     private bool CheckBigStructure(Vector3Int position, int width, int height)
     {
         var nearRoad = false;
@@ -43,35 +41,11 @@ public class StructureManager : MonoBehaviour
                 var newPosition = position + new Vector3Int(x, 0, z);
                 
                 if (DefaultCheck(newPosition)==false)
-                {
                     return false;
-                }
-                if (nearRoad == false)
-                {
-                    nearRoad = RoadCheck(newPosition);
-                }
+                if (nearRoad == false) nearRoad = RoadCheck(newPosition);
             }
         }
         return nearRoad;
-    }
-    public void PlaceSpecial(Vector3Int position)
-    {
-        if (!CheckPositionBeforePlacement(position)) return;
-        var randomIndex = GetRandomWeightedIndex(_specialWeights);
-        placementManager.PlaceObjectOnTheMap(position, specialPrefabs[randomIndex].prefab, CellType.SpecialStructure);
-    }
-    public static int GetRandomWeightedIndex(float[] weights)
-    {
-        var sum = weights.Sum();
-        var randomValue = UnityEngine.Random.Range(0, sum);
-        float tempSum = 0;
-        for (var i = 0; i < weights.Length; i++)
-        {
-            if(randomValue >= tempSum && randomValue < tempSum + weights[i])
-                return i;
-            tempSum += weights[i];
-        }
-        return 0;
     }
     private bool CheckPositionBeforePlacement(Vector3Int position) => DefaultCheck(position) && RoadCheck(position);
     private bool RoadCheck(Vector3Int position)
@@ -91,12 +65,4 @@ public class StructureManager : MonoBehaviour
         Debug.Log("This position is not EMPTY");
         return false;
     }
-}
-
-[Serializable]
-public struct StructurePrefabWeighted
-{
-    public GameObject prefab;
-    [Range(0,1)]
-    public float weight;
 }
