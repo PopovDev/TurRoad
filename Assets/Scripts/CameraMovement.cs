67fa09d422038b9109dac4dd0fb8966656cd45eb
Andrey <andreypopov2628007@gmail.com>
2021-09-28 14:37:58 +0300

    Added new control traffic objects

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,14 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Serialization;
 
 namespace SVS
 {
-
+    [ExecuteInEditMode]
     public class CameraMovement : MonoBehaviour
     {
         private Camera _gameCamera;
         [SerializeField]
         private float speed = 5f,maxSize = 5f,minSize = 0.5f,sensitivity = 10;
+        [SerializeField]
+        private bool camUp = true;
+        public event Action<bool> CamModeChanged;
         private void Start() => _gameCamera = GetComponent<Camera>();
 
         public void MoveCamera(Vector3 inputVector)
@@ -19,6 +23,7 @@ namespace SVS
 
         private void Update()
         {
+            CamModeChanged?.Invoke(camUp);
             var scrollInput = Input.GetAxis("Mouse ScrollWheel") * sensitivity;
             _gameCamera.orthographicSize = Mathf.Clamp(_gameCamera.orthographicSize - scrollInput, minSize, maxSize);
         }
